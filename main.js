ad48e4bc409e3e951dc22fd91c7e4792e8ea09be
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-20 16:57:29 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+import * as THREE from './build/three.module.js';
+import { OrbitControls } from './controls/OrbitControls.js';
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.18/+esm';
+
+/**
+ * UIデバッグ
+ */
+const gui = new GUI();
+
+//シーン
+const scene = new THREE.Scene();
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.set(1, 1, 2);
+
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
+
+//ジオメトリを作ってみよう。
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+//マテリアル
+const material = new THREE.MeshBasicMaterial({
+  color: 'red',
+});
+
+//メッシュ化
+const box = new THREE.Mesh(boxGeometry, material);
+scene.add(box);
+
+//ライト
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+//マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+window.addEventListener('resize', onWindowResize);
+
+const clock = new THREE.Clock();
+
+function animate() {
+  const elapsedTime = clock.getElapsedTime();
+
+  controls.update();
+
+  //レンダリング
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+//ブラウザのリサイズに対応
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+animate();
